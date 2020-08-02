2757b43159db60cc6fc718520b14cfbc22a8fa7e
riderx <martindonadieu@gmail.com>
2020-08-02 17:23:54 +0200

    make build lighter

--- webpack.config.js
+++ webpack.config.js
@@ -40,82 +40,110 @@ const targets = {
 }
 
 module.exports = env => {
-    return {
-            mode: 'production',
-    watch: env && env.production ? !env.production:  false,
-    devServer: {
-        inline: false,
-        writeToDisk: true,
-        publicPath: '/',
-        contentBase: path.join(__dirname, 'dist'),
-        port: 9000,
-        setup: function(app) {
-            app.post('/api/:id/login', proxyApi);
-            app.get('/api/:id/login', proxyApi);
-            app.use('/iframe/:id', (req, res) => proxy.createProxyMiddleware({ target: targets[req.params.id], changeOrigin: true })(req, res));
-        },
-    },
-    entry : {
-        test_parent: entryPointsPathPrefix + '/test_parent.ts',
-        test_child: entryPointsPathPrefix + '/test_child.ts',
-        trigger_child: entryPointsPathPrefix + '/trigger_child.ts',
-        jupyter: entryPointsPathPrefix + '/jupyter.ts',
-        wekan: entryPointsPathPrefix + '/wekan.ts',
-        toucan: entryPointsPathPrefix + '/toucan.ts',
-        healthcheck: entryPointsPathPrefix + '/healthcheck.ts',
-        filestash: entryPointsPathPrefix + '/filestash.ts'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: path.resolve(__dirname, 'public'),
-        library: 'bobRpa',
-        filename: '[name].js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json', '.css', '.html'],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-            { from: 'src/public', to: 'public' },
-            { from: 'package.json', to: 'package.json' },
-            ],
-        }),
-        new HtmlWebpackPlugin({
-            template: 'src/public/index.html',
-            filename: './index.html',
-        })
-    ],
-    module: {
-        rules: [{
-            // Include ts, tsx, js, and jsx files.
-            test: /\.(ts|js)x?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        },
-        {
-            test: /\.(png|jpg|gif)$/i,
-            use: [
-                {
-                loader: 'url-loader',
-                options: {
-                    limit: 8192,
-                },
-                },
-            ],
-            },
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        },
-        {
-            test: /\.html$/i,
-            loader: 'html-loader',
-        }
-        ],
+    let output = {};
+    let entry = {};
+    let plugins = [];
+    if (env && env.production) {
+        entry = {
+            jupyter: entryPointsPathPrefix + '/jupyter.ts',
+            wekan: entryPointsPathPrefix + '/wekan.ts',
+            toucan: entryPointsPathPrefix + '/toucan.ts',
+            healthcheck: entryPointsPathPrefix + '/healthcheck.ts',
+            filestash: entryPointsPathPrefix + '/filestash.ts'
+        };
+        output = {
+            path: path.resolve(__dirname, 'dist'),
+            library: 'bobRpa',
+            filename: '[name].js'
+        };
+        plugins = [
+            new CopyPlugin({
+                patterns: [
+                { from: 'package.json', to: 'package.json' },
+                ],
+            })
+        ];
+    } else {
+        entry = {
+            test_parent: entryPointsPathPrefix + '/test_parent.ts',
+            test_child: entryPointsPathPrefix + '/test_child.ts',
+            trigger_child: entryPointsPathPrefix + '/trigger_child.ts',
+            jupyter: entryPointsPathPrefix + '/jupyter.ts',
+            wekan: entryPointsPathPrefix + '/wekan.ts',
+            toucan: entryPointsPathPrefix + '/toucan.ts',
+            healthcheck: entryPointsPathPrefix + '/healthcheck.ts',
+            filestash: entryPointsPathPrefix + '/filestash.ts'
+        };
+        output = {
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: path.resolve(__dirname, 'public'),
+            library: 'bobRpa',
+            filename: '[name].js'
+        };
+        plugins = [
+            new CopyPlugin({
+                patterns: [
+                { from: 'src/public', to: 'public' },
+                { from: 'package.json', to: 'package.json' },
+                ],
+            }),
+            new HtmlWebpackPlugin({
+                template: 'src/public/index.html',
+                filename: './index.html',
+            })
+        ];
     }
-}
+    return {
+        mode: env && env.production ? 'production' : 'dev',
+        watch: env && env.production ? !env.production:  false,
+        devServer: {
+            inline: false,
+            writeToDisk: true,
+            publicPath: '/',
+            contentBase: path.join(__dirname, 'dist'),
+            port: 9000,
+            setup: function(app) {
+                app.post('/api/:id/login', proxyApi);
+                app.get('/api/:id/login', proxyApi);
+                app.use('/iframe/:id', (req, res) => proxy.createProxyMiddleware({ target: targets[req.params.id], changeOrigin: true })(req, res));
+            },
+        },
+        entry : entry,
+        output: output,
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.json', '.css', '.html'],
+        },
+        plugins: plugins,
+        module: {
+            rules: [{
+                // Include ts, tsx, js, and jsx files.
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                    },
+                    },
+                ],
+                },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            }
+            ],
+        }
+    }
 };
